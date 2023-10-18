112420455712fe235a464f30eb234ed59819a40f
dannygorgon <thirdoptionisthename@googlemail.com>
2023-10-18 22:43:41 +0100

    add styling and variables with listeners linked to buttons and sections on the UI

---
+++ script.js
@@ -0,0 +1,8 @@
+// dynamic images with listener ids
+const leftImage = document.getElementById("left-image");
+const rightImage = document.getElementById("right-image");
+
+//buttons with listener ids
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
