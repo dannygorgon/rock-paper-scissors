bec53d068ca174ff346c71aacb60d10ffec3e2f8
dannygorgon <thirdoptionisthename@googlemail.com>
2023-10-22 00:58:33 +0100

    add game logic for computer selection, created image array

--- script.js
+++ script.js
@@ -6,28 +6,60 @@ const rightImage = document.getElementById("right-image");
 const rockButton = document.getElementById("rock-button");
 const paperButton = document.getElementById("paper-button");
 const scissorsButton = document.getElementById("scissors-button");
-const startButton  =  document.getElementById("start-button");
+const changeText = document.querySelector("#change-text");
+const startButton = document.getElementById("start-button");
 
-rockButton.addEventListener("click", function() {
-    console.log("I am a rock")
-    leftImage.src = "rock.png"
-})
-
-paperButton.addEventListener("click", function() {
-    console.log("I am some paper")
-    leftImage.src = "paper.png"
-})
-
-scissorsButton.addEventListener("click", function() {
-    console.log("I am some scissors")
-    leftImage.src = "scissors.png"
-})
-
-startButton.addEventListener("click", function() {
-    console.log("Let the games begin")
-    leftImage.src = "default.png"
-    rightImage.src = "default.png"
-})
+//game logic
+const options = ["rock", "paper", "scissors"];
 
 
+//computer logic
 
+const imageArray = ["rock.png", "paper.png", "scissors.png"]
+
+rockButton.addEventListener("click", function () {
+  console.log("I am a rock");
+  leftImage.src = "rock.png";
+  const playerSelection = options[0];
+  changeText.textContent = `${playerSelection}`;
+  getComputerChoice();
+});
+
+paperButton.addEventListener("click", function () {
+  console.log("I am some paper");
+  leftImage.src = "paper.png";
+  const playerSelection = options[1];
+  changeText.textContent = `${playerSelection}`;
+
+  getComputerChoice();
+});
+
+scissorsButton.addEventListener("click", function () {
+  console.log("I am some scissors");
+  leftImage.src = "scissors.png";
+  const playerSelection = options[2];
+  changeText.textContent = `${playerSelection}`;
+
+  getComputerChoice();
+});
+
+startButton.addEventListener("click", function () {
+  console.log("Let the games begin");
+  leftImage.src = "default.png";
+  rightImage.src = "default.png";
+  changeText.textContent = "Make your choice!";
+});
+
+function getComputerChoice() {
+  const computerSelection = options[Math.floor(Math.random() * options.length)];
+  console.log(computerSelection);
+
+  return computerSelection;
+}
+
+function computerSelectionImage(){
+
+}
+
+
+function startGame() {}
