1cdac575d1a1287410ee6a6c5dfe70d512353b92
dannygorgon <thirdoptionisthename@googlemail.com>
2023-10-24 22:59:13 +0100

    rearrange functions to keep score externally, add resetGame and checkGameEnd states. add playRound

--- script.js
+++ script.js
@@ -1,90 +1,101 @@
-// dynamic images with listener ids
+//Dynamic images
 const leftImage = document.getElementById("left-image");
 const rightImage = document.getElementById("right-image");
 
-// buttons with listener ids
+// Button event listeners
 const rockButton = document.getElementById("rock-button");
 const paperButton = document.getElementById("paper-button");
 const scissorsButton = document.getElementById("scissors-button");
-const changeText = document.querySelector("#change-text");
 const startButton = document.getElementById("start-button");
 
-// game logic
+// Test changes
+const changeText = document.querySelector("#change-text");
+const changeScore = document.querySelector("#score-text");
+
+// Computer choice array
 const options = ["rock", "paper", "scissors"];
 
+//Scores and maxScore
+let playerScore = 0;
+let computerScore = 0;
+const maxScore = 5;
+
 rockButton.addEventListener("click", function () {
-  console.log("I am a rock");
-  leftImage.src = "rock.png";
-  const playerSelection = options[0];
-  const computerSelection = getComputerChoice();
-  const resultMessage = 
-  playerSelection === "rock" && computerSelection === "rock" ? "It's a draw!":
-  playerSelection === "rock" && computerSelection === "paper" ? "Computer wins!!" :
-  playerSelection === "rock" && computerSelection === "scissors" ?  "Player wins!!":
-  "";
-
-changeText.textContent = resultMessage;
-
+  if (playerScore < maxScore && computerScore < maxScore) { // if maxScore not reached
+    leftImage.src = "rock.png"; // change the left image
+    const playerSelection = options[0]; // store player selection
+    const computerSelection = getComputerChoice(); // call and store computerSelection
+    playRound(playerSelection, computerSelection); // output the choices to playRound
+    updateScore(); // if maxScore not reached, update score
+  }
+  checkGameEnd(); // cakl and check endGame state
 });
 
 paperButton.addEventListener("click", function () {
-  console.log("I am some paper");
-  leftImage.src = "paper.png";
-  const playerSelection = options[1];
-  const computerSelection = getComputerChoice();
-  const resultMessage = 
-  playerSelection === "paper" && computerSelection === "rock" ? "Player wins!!":
-  playerSelection === "paper" && computerSelection === "paper" ? "It's a draw!" :
-  playerSelection === "paper" && computerSelection === "scissors" ? "Computer wins!!" :
-  "";
-
-changeText.textContent = resultMessage;
+  if (playerScore < maxScore && computerScore < maxScore) {
+    leftImage.src = "paper.png";
+    const playerSelection = options[1];
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    updateScore();
+  }
+  checkGameEnd();
 });
 
 scissorsButton.addEventListener("click", function () {
-    console.log("I am some scissors");
+  if (playerScore < maxScore && computerScore < maxScore) {
     leftImage.src = "scissors.png";
     const playerSelection = options[2];
     const computerSelection = getComputerChoice();
-  
-    const resultMessage = 
-      playerSelection === "scissors" && computerSelection === "rock" ? "Computer wins!!" :
-      playerSelection === "scissors" && computerSelection === "paper" ? "Player wins!!" :
-      playerSelection === "scissors" && computerSelection === "scissors" ? "It's a draw!!" :
-      "";
-  
-    changeText.textContent = resultMessage;
-  });
-  
+    playRound(playerSelection, computerSelection);
+    updateScore();
+  }
+  checkGameEnd();
+});
 
 startButton.addEventListener("click", function () {
-  console.log("Let the games begin");
-  leftImage.src = "default.png";
-  rightImage.src = "default.png";
-  changeText.textContent = "Make your choice!";
+  resetGame(); // Reset game
 });
 
 function getComputerChoice() {
-  const computerSelection = options[Math.floor(Math.random() * options.length)];
-  computerSelectionImage(computerSelection);
-  computerSelectionText(computerSelection);
-
-  return computerSelection;
+  const computerSelection = options[Math.floor(Math.random() * options.length)]; // random computer choice
+  rightImage.src = computerSelection + ".png"; // change image to reflect computer choice
+  return computerSelection; //o
 }
 
-function computerSelectionImage(computerSelection) {
-  rightImage.src = computerSelection + ".png";
-}
-
-function computerSelectionText(computerSelection) {
-  let computerChoiceText = "";
-  if (computerSelection === "rock") {
-    computerChoiceText = "rock";
-  } else if (computerSelection === "paper") {
-    computerChoiceText = "paper";
-  } else if (computerSelection === "scissors") {
-    computerChoiceText = "scissors";
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) { // draw state
+    changeText.textContent = "It's a draw!";
+  } else if ( // player win state
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    changeText.textContent = "Player wins!";
+    playerScore++;
+  } else { // computer win state
+    changeText.textContent = "Computer wins!";
+    computerScore++;
   }
+}
 
-  return computerChoiceText;
+function updateScore() {
+  changeScore.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+}
+
+function checkGameEnd() {
+  if (playerScore === maxScore) {
+    changeText.textContent = "Player Wins! Press Start to Play Again";
+  } else if (computerScore === maxScore) {
+    changeText.textContent = "Computer Wins! Press Start to Play Again";
+  }
+}
+
+function resetGame() {
+  leftImage.src = "default.png";
+  rightImage.src = "default.png";
+  changeText.textContent = "Make your choice!";
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
 }
